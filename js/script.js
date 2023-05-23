c4d7f0f281962c9f0ab8c997905d3657cec1aba6
andresito-luna <andresluna007@gmail.com>
2023-05-23 01:20:12 -0300

    Completada validación de contacto.

--- js/script.js
+++ js/script.js
@@ -36,3 +36,48 @@ const llamada = () => {
 
 btn.addEventListener("click", llamada)
 refrescar.addEventListener("click", function () { location.reload() })
+
+
+
+
+// validador
+
+function validarFormulario() {
+    // Obtener los valores ingresados por el usuario y recortar
+    // los posibles espacios en blanco al principio y al final.
+    var nombre = document.getElementById("contacto__nombre").value.trim();
+    var dni = document.getElementById("dni").value.trim();
+    var direccion = document.getElementById("direccion").value.trim();
+
+    // Verificar si algún campo está en blanco
+    if (nombre === "" || dni === "" || direccion === "") {
+      alert("Por favor, complete todos los campos del formulario.");
+      return false;
+    }
+
+    // Verificar si el nombre contiene solo caracteres alfabéticos y espacios
+    for (var i = 0; i < nombre.length; i++) {
+      var charCode = nombre.charCodeAt(i);
+      if (!((charCode >= 65 && charCode <= 90) || (charCode >= 97 && charCode <= 122) || charCode === 32)) {
+        alert("El campo 'nombre' solo puede contener caracteres alfabéticos y espacios.");
+        return false;
+      }
+    }
+
+    // Verificar si el DNI contiene solo 8 dígitos numéricos
+    if (dni.length !== 8) {
+      alert("El campo 'dni' debe contener exactamente 8 dígitos numéricos.");
+      return false;
+    }
+    for (var j = 0; j < dni.length; j++) {
+      var digit = dni.charAt(j);
+      if (digit < "0" || digit > "9") {
+        alert("El campo 'dni' solo puede contener dígitos numéricos.");
+        return false;
+      }
+    }
+
+    // Si todas las validaciones son exitosas, enviar el formulario
+    alert("Formulario enviado correctamente.");
+    return true;
+  }
